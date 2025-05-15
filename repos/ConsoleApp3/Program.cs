0b51dc86f0be8e5f8c34ab1080a81fb1a70a73f8
Atharv Mannur <zcon.adl@gmail.com>
2025-05-15 12:25:02 +0530

    Added reference to the project

---
+++ repos/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Applicationcreation.Services;
+using Applicationcreation.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Load configuration
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var user = new User
+        {
+            Id = 6,
+            Email = "test@gmail.com",
+            Role = new Role { Name = "Admin" }
+        };
+
+        //Initialize and use TokenService
+        var tokenService = new TokenService(config);
+        var token = tokenService.GenerateJwtToken(user);
+
+        Console.WriteLine("Generated Token:");
+        Console.WriteLine(token);
+    }
+}
